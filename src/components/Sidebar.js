d066143925959c35d8e8bdc4e0c4e1392ca5aba6
unknown <songpitou723@gmai.com>
2022-12-07 22:31:45 -0500

    sidebar change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
-// import mouse from "./../music/mouse.mp3";
 import { SidebarData } from "./SidebarData";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 function SideBar() {
   const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/8");
-
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   const clickHandler = () => {
     audio.play();
   };
   return (
     <>
       <div className="navbar">
+        <Link to="#" className={sidebar ? "menu-bar" : "menu-bar show"}>
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
         <h1 className="title">Lofi Player</h1>
       </div>
-      <nav className="Sidebar">
-        <ul className="SidebarList">
+
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+
           {SidebarData.map((val, key) => {
             return (
-              <li key={val.id} className="row">
+              <li key={val.id} className="nav-item">
                 <Link to={val.link} onClick={clickHandler}>
                   <span className="item">
                     {/* <span id="dot">. </span> */}
